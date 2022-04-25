f5cb79773ee7b5baf72f7f351e1aa50a5971e670
username <youremail@domain.com>
2022-04-25 14:57:47 -0700

    detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Bird(models.Model):
+    name = models.CharField(max_length=100)
+    specie = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    age = models.IntegerField()
+
+
+    def __str__(self):
+        return f"The bird named {self.name} has id of {self.id}"
--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse #res.send in express
+from .models import Bird
 
-class Bird:
-    def __init__(self, name, specie, description, age):
-        self.name = name
-        self.specie = specie
-        self.description = description
-        self.age = age
-
-
-birds = [
-    Bird('Zazu', 'African red-billed hornbill', 'from Lion King', 'Adult'),
-    Bird('Slinky', 'Parrot', 'colorful red, blue and green', 'Juvenile'),
-    Bird('Scooter', 'Hummingbird', 'long beak, shiny green belly', 0),
-    Bird('Rio', 'Macaw', 'multiple colors red, green, orange, yellow', 'Adult'),
-    Bird('Tweety', 'Yellow canary', 'yellow, black feathers', 'Juvenile')
-]
 
 
 
@@ -29,4 +15,9 @@ def about(request):
     return render(request, 'about.html')
 
 def birds_index(request):
+    birds = Bird.objects.all()
     return render(request, 'birds/index.html', {'birds': birds})
+
+def birds_detail(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html', {'bird': bird})
